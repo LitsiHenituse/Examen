4504eac82e979824ab2f6723eaf29df10ea083fd
LitsiHenituse <82629927+LitsiHenituse@users.noreply.github.com>
2021-06-01 20:43:24 -0500

    Add files via upload

---
+++ Modern-Media-Player-UI-C-Sharp-master/PlayerUI/Form4.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace ReproductorMusicaLitsiZ
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
+            this.Reproductor = new AxWMPLib.AxWindowsMediaPlayer();
+            this.listCanciones = new System.Windows.Forms.ListBox();
+            this.btnAdjuntar = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Reproductor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAdjuntar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Reproductor
+            // 
+            this.Reproductor.Enabled = true;
+            this.Reproductor.Location = new System.Drawing.Point(175, 37);
+            this.Reproductor.Name = "Reproductor";
+            this.Reproductor.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Reproductor.OcxState")));
+            this.Reproductor.Size = new System.Drawing.Size(380, 46);
+            this.Reproductor.TabIndex = 0;
+            // 
+            // listCanciones
+            // 
+            this.listCanciones.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.listCanciones.FormattingEnabled = true;
+            this.listCanciones.Location = new System.Drawing.Point(101, 104);
+            this.listCanciones.Name = "listCanciones";
+            this.listCanciones.Size = new System.Drawing.Size(503, 264);
+            this.listCanciones.TabIndex = 1;
+            // 
+            // btnAdjuntar
+            // 
+            this.btnAdjuntar.Image = ((System.Drawing.Image)(resources.GetObject("btnAdjuntar.Image")));
+            this.btnAdjuntar.Location = new System.Drawing.Point(26, 21);
+            this.btnAdjuntar.Name = "btnAdjuntar";
+            this.btnAdjuntar.Size = new System.Drawing.Size(100, 53);
+            this.btnAdjuntar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnAdjuntar.TabIndex = 2;
+            this.btnAdjuntar.TabStop = false;
+            this.btnAdjuntar.Click += new System.EventHandler(this.btnAdjuntar_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.ClientSize = new System.Drawing.Size(668, 392);
+            this.Controls.Add(this.btnAdjuntar);
+            this.Controls.Add(this.listCanciones);
+            this.Controls.Add(this.Reproductor);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Reproductor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAdjuntar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer Reproductor;
+        private System.Windows.Forms.ListBox listCanciones;
+        private System.Windows.Forms.PictureBox btnAdjuntar;
+    }
+}
---
+++ Modern-Media-Player-UI-C-Sharp-master/PlayerUI/Form4.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReproductorMusicaLitsiZ
+{
+    public partial class Form4 : Form
+    {
+
+        bool Play = false;
+        string[] ArchivoMP3;
+        string[] rutasArchivosMP3;
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void btnAdjuntar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog CajaDeBusquedaDeArchivos = new OpenFileDialog();
+            CajaDeBusquedaDeArchivos.Multiselect = true;
+            if (CajaDeBusquedaDeArchivos.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ArchivoMP3 = CajaDeBusquedaDeArchivos.SafeFileNames;
+                rutasArchivosMP3 = CajaDeBusquedaDeArchivos.FileNames;
+                foreach (var ArchivoMP3 in ArchivoMP3)
+                {
+                    listCanciones.Items.Add(ArchivoMP3);
+                }
+                Reproductor.URL = rutasArchivosMP3[0];
+            }
+        }
+
+        private void listCanciones_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Reproductor.URL = rutasArchivosMP3[listCanciones.SelectedIndex];
+        }
+    }
+    }
+}
